fc1e4c0304cb94f53f5dd407589e6d77a09ceb9e
MD Mehedi Hasan <mehedih2204@gmail.com>
2022-06-07 22:36:00 +0600

    Initial commit

---
+++ 4A_Watermelon.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int w;
+    cin>>w;
+    if (w%2==0 && w>2)
+    {
+        std::cout << "YES" << std::endl;
+    }
+    else
+    {
+        std::cout << "NO" << std::endl;
+    }
+    
+    
+    return 0;
+}
